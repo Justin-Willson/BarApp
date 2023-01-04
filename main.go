a61829d93664f80d8f1af406eb88b9987c062711
Justin Willson <jt.will.525@gmail.com>
2023-01-04 15:16:28 -0700

    fixed CORS issue with API

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Justin-Willson/BarApp/app"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	router.GET("/ingredients", app.GetIngredients)
 	router.GET("/cocktails", app.GetCocktails)
 	router.POST("/ingredients", app.AddIngredient)
