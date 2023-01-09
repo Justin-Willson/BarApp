062c7c2c7536a3f110f34bd9507aa2f7cb0ce3f1
Justin Willson <jt.will.525@gmail.com>
2023-01-09 14:58:56 -0700

    Removed dependency on bootstrap-vue and added vanilla bootstrap

--- web/BarAppUI/src/main.ts
+++ web/BarAppUI/src/main.ts
@@ -1,18 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import BootstrapVue3 from 'bootstrap-vue-3'
 
 import './assets/main.css'
+import "bootstrap"
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 import IngredientTableVue from './components/IngredientTable.vue'
 import IngredientRowVue from './components/IngredientRow.vue'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(BootstrapVue3)
 
 app.component('IngredientTable', IngredientTableVue)
 app.component('IngredientRow', IngredientRowVue)
